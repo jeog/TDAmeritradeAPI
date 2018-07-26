12d1749918c34c7a8b83491c37cec87d9d68e86a
Jonathon <jeog.dev@gmail.com>
2018-07-26 13:14:04 -0400

    update release build makefiles

---
+++ Release/src/getters/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/getters/account.cpp \
+../src/getters/historical.cpp \
+../src/getters/instrument_info.cpp \
+../src/getters/market_hours.cpp \
+../src/getters/movers.cpp \
+../src/getters/options.cpp \
+../src/getters/quotes.cpp 
+
+OBJS += \
+./src/getters/account.o \
+./src/getters/historical.o \
+./src/getters/instrument_info.o \
+./src/getters/market_hours.o \
+./src/getters/movers.o \
+./src/getters/options.o \
+./src/getters/quotes.o 
+
+CPP_DEPS += \
+./src/getters/account.d \
+./src/getters/historical.d \
+./src/getters/instrument_info.d \
+./src/getters/market_hours.d \
+./src/getters/movers.d \
+./src/getters/options.d \
+./src/getters/quotes.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/getters/%.o: ../src/getters/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -DTHIS_EXPORTS_INTERFACE -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -4,38 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/account.cpp \
 ../src/auth.cpp \
 ../src/curl_connect.cpp \
-../src/data.cpp \
 ../src/params.cpp \
-../src/query.cpp \
 ../src/tdma_connect.cpp \
-../src/trade.cpp \
 ../src/util.cpp \
 ../src/websocket_connect.cpp 
 
 OBJS += \
-./src/account.o \
 ./src/auth.o \
 ./src/curl_connect.o \
-./src/data.o \
 ./src/params.o \
-./src/query.o \
 ./src/tdma_connect.o \
-./src/trade.o \
 ./src/util.o \
 ./src/websocket_connect.o 
 
 CPP_DEPS += \
-./src/account.d \
 ./src/auth.d \
 ./src/curl_connect.d \
-./src/data.d \
 ./src/params.d \
-./src/query.d \
 ./src/tdma_connect.d \
-./src/trade.d \
 ./src/util.d \
 ./src/websocket_connect.d 
 
